fa1bc6eae68b0856da9d170d2781a81f1b56993a
namo <namo@namo-900X3N>
2017-05-19 12:30:16 +0900

    관리자 게시판 생성 0519

--- project0515/src/project/jyland/controller/CategoryController.java
+++ project0515/src/project/jyland/controller/CategoryController.java
@@ -1,10 +1,16 @@
 package project.jyland.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import project.jyland.board.model.JYCat;
 import project.jyland.category.dao.CategoryService;
 
 @Controller
@@ -14,5 +20,12 @@ public class CategoryController {
 	
 	@Autowired
 	private CategoryService categoryService;
+	
+	@RequestMapping(value = "createboard.jy", method = RequestMethod.POST)
+	public String createboard(JYCat cat, Model model) {
+		logger.info("willkommen CategoryController createboard " + new Date());
+		categoryService.createCategory(cat);
+		return "redirect:/myinfo.jy";
+	}
 
 }
